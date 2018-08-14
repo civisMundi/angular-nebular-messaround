1ab5b5e8199f40dc5dacd6339b5de241c99aecdd
dydyFos <dylanfoster.esgi@gmail.com>
2018-08-15 01:33:20 +0200

    login premisces in user feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './app.effects';
+import { LoginComponent } from './user/pages/login/login.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        LoginComponent,
     ],
     imports: [
         BrowserModule,
