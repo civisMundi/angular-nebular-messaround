abb4d90c7d08c0f3100cdec172b1691046664912
dydyFos <dylanfoster.esgi@gmail.com>
2018-08-14 23:19:01 +0200

    init

---
+++ src/app/app.effects.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Observable } from 'rxjs';
+
+import { AppEffects } from './app.effects';
+
+describe('AppEffects', () => {
+    let actions$: Observable<any>; // tslint:disable-line
+    let effects: AppEffects;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                AppEffects,
+                provideMockActions(() => actions$)
+            ]
+        });
+        effects = TestBed.get(AppEffects);
+    });
+
+    it('should be created', () => {
+        expect(effects).toBeTruthy();
+    });
+});
